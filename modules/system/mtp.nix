e2d1515ce208b5a3bf41a223fe5829b33c1e372e
egocoder <alandeassisgoncalvessantos@gmail.com>
2025-09-18 01:15:23 -0300

    feat(system): initial modular system configuration

--- modules/system/mtp.nix
+++ modules/system/mtp.nix
@@ -1,11 +1,18 @@
-services.gvfs.enable = true;
-services.udisks2.enable = true;
+{ config, pkgs, ... }:
 
-environment.systemPackages = with pkgs; [
-  jmtpfs
-  simple-mtpfs
-  gvfs
-  kdePackages.kio-extras
-];
+{
+  # Enable MTP/USB services
+  services.gvfs.enable = true;       # Virtual filesystem for devices
+  services.udisks2.enable = true;    # Disk management service
 
-programs.adb.enable = true;
+  # System packages needed for MTP and USB devices
+  environment.systemPackages = with pkgs; [
+    jmtpfs
+    simple-mtpfs
+    gvfs
+    kdePackages.kio-extras  # Extra KIO plugins (optional)
+  ];
+
+  # Enable Android Debug Bridge (ADB)
+  programs.adb.enable = true;
+}
