de2bf1067d937ca47ce25287fa5146a0d0622587
Ego <166788669+egocoder@users.noreply.github.com>
2025-10-06 17:40:33 -0300

    test

---
+++ config.nix
@@ -0,0 +1,13 @@
+{
+  # -- User Details --
+  username = "weaver";
+
+  # -- System Details --
+  hostname = "loom";
+
+  gpuVendor = "nvidia";
+
+  # -- Optional Future Settings --
+  # email = "username@example.com";
+  # gitUsername = "user-git";
+}
