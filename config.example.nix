9d0cfa4c1994bbf443a87e952c97367ae965c06d
egocoder <alandeassisgoncalvessantos@gmail.com>
2025-10-06 11:27:03 -0300

    gpu config

--- config.example.nix
+++ config.example.nix
@@ -4,4 +4,10 @@
 
   # -- System Details --
   hostname = "your-hostname-here";
+
+  gpuVendor = "nvidia";
+
+  # -- Optional Future Settings --
+  # email = "username@example.com";
+  # gitUsername = "user-git";
 }
