d7f1943b4f782f2f7dce834ca2da1c8117de94f3
egocoder <egocoderx@gmail.com>
2025-10-23 10:49:00 -0300

    helix + configs

--- modules/home/editors/helix.nix
+++ modules/home/editors/helix.nix
@@ -4,5 +4,36 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
+
+    extraPackages = with pkgs; [
+      nil
+      nixpkgs-fmt
+      rust-analyzer
+      ty
+      markdown-oxide
+      superhtml
+      tombi
+    ];
+
+    settings = {
+
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+        scrolloff = 5;
+        mouse = false;
+        auto-completion = true;
+      };
+    };
+
+    languages = {
+      language = [
+        { name = "nix"; formatter = { command = "nixpkgs-fmt"; }; }
+        { name = "rust";}
+        { name = "python"; formatter = { command = "ty"; }; }
+        { name = "markdown"; formatter = { command = "markdown-oxide"; }; }
+      ];
+    };
+    
   };
 }
