21c9c1883e20e5ced0d3e71926873ca22a00cffc
egocoder <egocoderx@gmail.com>
2025-10-23 10:47:16 -0300

    removed xwaylandobridge

--- modules/home/packages/hyprland-deps.nix
+++ modules/home/packages/hyprland-deps.nix
@@ -2,8 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    kdePackages.xwaylandvideobridge
     libnotify
-    # xdg-desktop-portal-gtk # REMOVIDO: Agora é gerenciado por modules/home/portals.nix
+    xdg-desktop-portal-gtk
   ];
 }
